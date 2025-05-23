c47ce4492b220d8ebe5856558d1c5eb4ec935be5
Keiran Pillman <kpillman@mag.rochester.edu>
2025-05-23 09:56:09 -0400

    added plugin main file

---
+++ rest-tests.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Plugin Name:       REST Tests
+ * Plugin URI:        https://github.com/keircatenation/rest-tests
+ * GitHub Plugin URI: https://github.com/keircatenation/rest-tests
+ * Description:       Code testing various aspects of WordPress's REST API.
+ * Version:           1.0.1
+ * Author:            Keiran Pillman
+ * License:           GPL version 3 or any later version
+ * License URI:       https://www.gnu.org/licenses/gpl-3.0.html
+ * Text Domain:       rest-tests
+ *
+ *     This plugin is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     any later version.
+ *
+ *     This plugin is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *     GNU General Public License for more details.
+ */
