2570a25c78a2637a1ee38b3bb778b609f07d262f
Keiran Pillman <kpillman@mag.rochester.edu>
2025-05-23 13:12:07 -0400

    added CPT

--- rest-tests.php
+++ rest-tests.php
@@ -20,3 +20,63 @@
  *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  *     GNU General Public License for more details.
  */
+
+
+// rest_prepare_{$this->post_type}
+add_filter( 'rest_prepare_post', function( $response, $post, $request ){
+    $image = array(
+        'html' => get_the_post_thumbnail( $post->ID ),
+        'url' => wp_get_attachment_image_src( $response->data['featured_media'], 'large' ),
+        'alt' => get_post_meta( $response->data['featured_media'], TRUE )
+    );
+    $response->data['image'] = $image;
+    // $response->data['request_params'] = $request->get_params();
+    // $response->data['title'] = $response->data['title']['rendered'] . 'addition';
+    return $response;
+}, 10, 3 );
+
+// rest_pre_echo_response
+// add_filter( 'rest_pre_echo_response', function( $result, $server, $request ){
+//     $result[] = $request->get_params();
+//     return $result;
+// }, 10, 3 );
+
+
+// custom post type
+add_action( 'init', function() {
+    register_post_type( 'fun_posts', array(
+        'label'               => __( 'fun posts', 'rest-tests' ),
+        'labels'              => array(
+            'name'                => __( 'fun posts', 'Post Type General Name', 'rest-tests' ),
+            'singular_name'       => __( 'fun post', 'Post Type Singular Name', 'rest-tests' ),
+            'menu_name'           => __( 'fun posts', 'rest-tests' ),
+            'parent_item_colon'   => __( 'Parent fun post', 'rest-tests' ),
+            'all_items'           => __( 'All fun posts', 'rest-tests' ),
+            'view_item'           => __( 'View fun post', 'rest-tests' ),
+            'add_new_item'        => __( 'Add New fun post', 'rest-tests' ),
+            'add_new'             => __( 'Add New fun post', 'rest-tests' ),
+            'edit_item'           => __( 'Edit fun post', 'rest-tests' ),
+            'update_item'         => __( 'Update fun post', 'rest-tests' ),
+            'search_items'        => __( 'Search fun posts', 'rest-tests' ),
+            'not_found'           => __( 'Not Found', 'rest-tests' ),
+            'not_found_in_trash'  => __( 'Not found in Trash', 'rest-tests' ),
+        ),
+        'supports'            => array( 'title', 'editor', 'excerpt', 'thumbnail', 'revisions', 'custom-fields', ),
+        'public'              => true,
+        'menu_position'       => 5,
+        'menu_icon'           => 'dashicons-bank',
+        'capability_type'     => 'post',
+        'show_in_rest'        => true,
+        'rewrite'             => array( 'slug' => 'fun-posts' ),
+        'taxonomies'          => [''],
+    ) );
+}, 0 );
+
+add_action( 'init', function(){
+    register_post_meta( 'fun_posts', 'fun_post_meta', array(
+        'type' => 'string',
+        'label' => 'Fun Post Meta',
+        'show_in_rest' => true,
+        'single' => true,
+    ) );
+} );
